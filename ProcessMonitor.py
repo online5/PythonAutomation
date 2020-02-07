96b3098807b87d71696ee194b025f31e0192d954
Deepak Patil <40465321+online5@users.noreply.github.com>
2020-02-07 22:38:52 +0530

    This python script to list down processes running.

---
+++ ProcessMonitor.py
@@ -0,0 +1,22 @@
+import psutil
+
+def ProcessDisplay():
+    listprocess =[]
+
+    for proc in psutil.process_iter():
+        try:
+            pinfo=proc.as_dict(attrs=['pid','name','username'])
+            listprocess.append(pinfo);
+        except(psutil.NoSuchProcess,psutil.AccessDenied,psutil.ZombieProcess):
+            pass
+
+    return listprocess;
+
+def main():
+    print("Process Monitor")
+    list=ProcessDisplay()
+
+    for elem in list:
+        print(elem)
+if __name__ =="__main__":
+  main();
